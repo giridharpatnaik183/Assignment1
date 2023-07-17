38ab43314a06ab70549a0c70b43b2cd41ff08146
pragati970 <134931794+pragati970@users.noreply.github.com>
2023-07-17 15:26:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  access_key = "AKIAVPA7N6UH3X5PDDNT"
-  secret_key = "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"
-  region     = "eu-north-1"
+access_key= "AKIAVPA7N6UH3X5PDDNT"
+secret_key= "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"
+region= "eu-north-1"
 }
-resource "aws_s3_bucket" "first" {
-  bucket = "my-unique-test-s3-bucket"
+resource "aws_s3_bucket" "third" {
+bucket = "pragati.prod.ec2.bucket"
 }
