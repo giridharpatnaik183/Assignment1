9d064f7951f61e4fc1f3ec63b5940169c36eec52
giridharpatnaik183 <94623129+giridharpatnaik183@users.noreply.github.com>
2023-07-18 15:43:46 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-access_key= "AKIAWS2ZE5YLEXO3PRNP"
-secret_key= "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"
-region= "eu-north-1"
+access_key= "AKIAWS2ZE5YLECGO4KIT"
+secret_key= "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"
+region= "us-east-1"
 }
-resource "aws_s3_bucket" "third" {
+resource "aws_s3_bucket" "first" {
 bucket = "sai.prod.ec2.bucket"
 }
