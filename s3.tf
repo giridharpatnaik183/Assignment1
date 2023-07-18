880d34ea0c89aff171bd4963b651330d852ad7aa
giridharpatnaik183 <94623129+giridharpatnaik183@users.noreply.github.com>
2023-07-18 10:44:01 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,13 +6,3 @@ region= "eu-north-1"
 resource "aws_s3_bucket" "third" {
 bucket = "sai.prod.ec2.bucket"
 }
-
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
