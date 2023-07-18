9940998c49db47867e52ea5bcae05cf327b28779
giridharpatnaik183 <94623129+giridharpatnaik183@users.noreply.github.com>
2023-07-18 14:37:08 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,15 +1,8 @@
 provider "aws" {
-  access_key = "AKIAWS2ZE5YLEXO3PRNP"
-  secret_key = "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"
-  region     = "eu-north-1"
+access_key= "AKIAWS2ZE5YLEXO3PRNP"
+secret_key= "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"
+region= "eu-north-1"
 }
-
-resource "aws_s3_bucket" "first" {
-  bucket = "sai.prod.ec2.bucket"
-}
-
-resource "aws_instance" "myvm" {
-  ami           = "ami-0989fb15ce71ba39e"
-  instance_type = "t3.micro"
-  key_name      = "Assignmenthoast"
+resource "aws_s3_bucket" "third" {
+bucket = "sai.prod.ec2.bucket"
 }
