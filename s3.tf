622eed8d056eca3284cb03f331eddb3cc373fcf6
giridharpatnaik183 <94623129+giridharpatnaik183@users.noreply.github.com>
2023-07-19 10:34:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,3 +6,11 @@ region= "us-east-1"
 resource "aws_s3_bucket" "first" {
 bucket = "sai.prod.s3.bucket"
 }
+resource "aws_instance" "myvm" {
+
+ 
+ami = "ami-053b0d53c279acc90"
+instance_type = "t3.micro"
+key_name = "sai1.0"
+
+}
