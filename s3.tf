1589b767422f69b19ff100944aa3aeb0f2dcd6cd
Giridhar Sai Patnaik <94623129+giridharpatnaik183@users.noreply.github.com>
2023-07-21 11:57:20 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-access_key= "AKIAWS2ZE5YLHTVIBMPD"
-secret_key= "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"
+access_key= ""
+secret_key= ""
 region= "us-east-2"
 }
 resource "aws_s3_bucket" "first" {
